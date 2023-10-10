0eef4bddbcd86408d83c45fe66a5284efa8dcc46
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2023-10-10 20:30:25 +0500

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -395,7 +395,7 @@ async def start(client, message):
                     InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
                 ]]
                 await message.reply_text(
-                    text="<b>You are not verified !\nKindly verify to continue !</b>",
+                    text="<b>You are not verified !\nKindly verify to get direct files for the next 24hrs 😇 How to Verify?- t.me/starvibehowto/42 !</b>",
                     protect_content=True,
                     reply_markup=InlineKeyboardMarkup(btn)
                 )
@@ -457,7 +457,7 @@ async def start(client, message):
             InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
         ]]
         await message.reply_text(
-            text="<b>You are not verified !\nKindly verify to continue !</b>",
+            text="<b>You are not verified !\nKindly verify to get direct files for the next 24hrs 😇 How to Verify t.me/starvibehowto/42 !</b>",
             protect_content=True,
             reply_markup=InlineKeyboardMarkup(btn)
         )
